33db7a2b33d1ede062177adebc9e6357cd209e1d
o0pmitev <pmitev89@gmail.com>
2023-12-19 06:47:20 +0200

    fix: add basic powerup system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public bool hasPowerUp = false;
+
     private Rigidbody playerRigidBody;
     private GameObject focalPoint;
+    private float knockbackForce = 15f;
     private void Start()
     {
         playerRigidBody = GetComponent<Rigidbody>();
@@ -19,5 +22,27 @@ public class PlayerController : MonoBehaviour
 
         playerRigidBody.AddForce(focalPoint.transform.forward * speed * forwardInput * Time.deltaTime);
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            Destroy(other.gameObject);
+            hasPowerUp = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 knockbackDirection = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidBody.AddForce(knockbackDirection * knockbackForce, ForceMode.Impulse);
+
+            Debug.Log("A player collided with: " + collision.gameObject + ". And has powerUp set to: " + hasPowerUp);
+        }
     }
 }
